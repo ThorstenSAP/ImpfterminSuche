9b8c89173103f6e8962dae9afbc075d732673e9e
Thorsten Müller <thorsten.mueller@js-soft.com>
2021-06-01 14:18:47 +0200

    chore: use methods

--- getCode.js
+++ getCode.js
@@ -11,15 +11,27 @@ const timerClicks = 20000 //timer between clicking on the page
 const timerResult = 300000 //time on result screen
 
 
-const getCodeBtn = '/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div'
+const resultBoxXPath = '/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div'
+const getCodeBtnXPath = '/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]'
 
 
 function playSound(){
     const player = require('play-sound')(opts = {})
     player.play(song)
 }
+async function validateResult(driver, element){
+    if (!element){
+        element = driver.findElement(By.xpath(resultBoxXPath))
+    } 
+    let resultText = await element.getText()
+    if (resultText.includes('Es wurden keine freien Termine in Ihrer Region gefunden. Bitte probieren Sie es später erneut.')) {
+        return false
+    } else { 
+        return true
+    } 
+}
 
-(async function example() {
+(async function main() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
             // Navigate to Url
@@ -39,42 +51,30 @@ function playSound(){
 
         //get 'button' Nein - Anspruch prüfen and click it
         await driver.sleep(timerClicks)
-        const cheddar = driver.findElement(By.xpath('/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[2]/div/div/label[2]'));
+        const cheddar = driver.findElement(By.xpath(getCodeBtnXPath));
         cheddar.click()
 
         //wait a few seconds
         await driver.sleep(timerClicks)
 
-        let bKeyPossible
-        
-        //get result element
-        let result = driver.findElement(By.xpath('/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div'));
-        let resultText = await result.getText()
-        if (resultText.includes('Es wurden keine freien Termine in Ihrer Region gefunden. Bitte probieren Sie es später erneut.')) {
-            bKeyPossible = false
-        } else { 
-            bKeyPossible = true
-        } 
+        let bKeyPossible = await validateResult(driver)
+ 
 
         while(!bKeyPossible){
             cheddar.click()
             await driver.sleep(timerClicks)
             try{
-                result = await driver.findElement(By.xpath('/html/body/app-root/div/app-page-its-login/div/div/div[2]/app-its-login-user/div/div/app-corona-vaccination/div[3]/div/div/div/div[2]/div/div/div'));
+                await driver.findElement(By.xpath(resultBoxXPath));
             } catch(e){
                 playSound()
                 await driver.sleep(timerResult) // wait at screen for 5 min
             }
             
-            let resultText = await result.getText()
-            if (resultText.includes('Es wurden keine freien Termine in Ihrer Region gefunden. Bitte probieren Sie es später erneut.')) {
-                bKeyPossible = false
-                console.log('no possible key')
-            } else { 
-                bKeyPossible = true
-            } 
+            bKeyPossible = await validateResult(driver) 
         }
         playSound()
+    }
+    catch(e){
     }
     finally{
         // driver.quit();
